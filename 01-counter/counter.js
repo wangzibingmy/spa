900bd9abb98544e0c220c76f1b69e62b8866017f
wangzibingmy <2119285613@qq.com>
2018-03-08 21:22:11 +0800

    counter change

--- 01-counter/counter.js
+++ 01-counter/counter.js
@@ -5,7 +5,9 @@ var z=document.getElementById('z');
 var m=document.getElementById('m');
 var btn=document.getElementById('btn');
 var re=/^[+]{0,1}(\d+)$|^[+]{0,1}(\d+\.\d+)$/;
-
+function roundFractional(x, n) {
+  return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);          
+}
 w.onblur=function(){
       if(w.value==""){      
             kk.innerHTML="请填写必要信息";
@@ -53,8 +55,10 @@ btn.onclick=function(){
   }else{
               gg.innerHTML="";
               kk.innerHTML="";
-              z.value=(w.value)*2+(h.value)*2;
-              m.value=w.value*h.value;                                  
+              z.value=roundFractional((w.value)*2+(h.value)*2,1);
+              m.value=roundFractional(w.value*h.value,2);                                  
   }
 }
-
+window.onload=function(){
+  w.focus();
+}
