1928f4ea5edaf56b26ff7cf4d86d6f474d08c3c2
wangzibingmy <2119285613@qq.com>
2018-03-13 16:56:42 +0800

    change

--- 05-progress/progress.js
+++ 05-progress/progress.js
@@ -3,24 +3,24 @@ var start=document.getElementById('start');
 var stop=document.getElementById('stop');
 var reset=document.getElementById('reset');
 var time=0;
-var i=0;  
+var max=pg.max;  
 start.onclick=function(){
-  if(pg.value!==0){
-    time=window.setInterval(function(){
-      pg.value+=1;
+  if(time!==0){
+  }else{
+    time=setInterval(function(){
+      if(pg.value==max){
+      clearInterval(time);
+      console('下载完成')；
+      }
+      pg.value++;
     },100);
-    return;
   }
-  time=window.setInterval(function(){
-    pg.value=i++;
-  },100);
 }
 stop.onclick=function(){
-  window.clearInterval(time);
+  clearInterval(time);
   time=0;
                   
 }
 reset.onclick=function(){
-  pg.value=0;     
-  i=0;          
+  pg.value=0;               
 }
